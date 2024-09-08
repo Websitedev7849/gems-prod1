9b7ed23c500a07eff3c024e8db57e162a6d1dbf4
Siddharth Pawar <websitedev7849@gmail.com>
2024-09-08 20:03:10 +0530

    setup a /handle-form endpoint to receive form-data

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/handle-form", (req, res) => {
+    console.log(req.query)
+    res.redirect("/")
+    
+})
+
 app.listen(PORT, () => {
     console.log(__dirname);  
 })
