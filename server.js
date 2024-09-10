1bdb750c557197cb0827a9d11fc9a3c7bed5b26b
Siddharth Pawar <websitedev7849@gmail.com>
2024-09-10 15:31:17 +0530

    Success fully able to receive query from frontend and send to email server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require("dotenv").config()
+const { sendEmail } = require("./mailHandler")
+
 const express = require('express')
+
 
 const app = express()
 
-const PORT = 5501
+const PORT = process.env.PORT || 5501
 
 app.use( express.static("public") )
 
@@ -10,12 +14,20 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.get("/handle-form", (req, res) => {
-    console.log(req.query)
-    res.redirect("/")
+app.get("/handle-form", async (req, res) => {
+
+    try {
+        // This below function will send an email to Admin
+        await sendEmail(req.query)
+        res.redirect("/")
+    } catch (error) {
+        console.log(error);
+        res.end("<h1>Error in sending mail</h1>").status(501)
+    }
+    
     
 })
 
 app.listen(PORT, () => {
-    console.log(__dirname);  
+    console.log(`Server Started PORT: ${PORT}`);  
 })
