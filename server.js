381480cfe3691b9b85ab1994dfe61ef1b4f4853b
Siddharth Pawar <websitedev7849@gmail.com>
2025-01-13 13:37:16 +0530

    add path of SSL certificates in env files

--- server.js
+++ server.js
@@ -181,8 +181,8 @@ app.get("/:page", (req, res) => {
 })
 
 const SSLServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+    key: fs.readFileSync(process.env.CERT_KEY_PATH),
+    cert: fs.readFileSync(process.env.CERT_PATH),
 }, app)
 
 SSLServer.listen(PORT, () => { console.log(`Listening on Port:${PORT}`)});
